fade83662a71e979e2c8372366615f7531fe1375
briri <brian.riley@ucop.edu>
2025-07-16 09:51:35 -0700

    fix merge conflict residue

--- gems/uc3-dmp-citation/lib/uc3-dmp-citation/version.rb
+++ gems/uc3-dmp-citation/lib/uc3-dmp-citation/version.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
 module Uc3DmpCitation
-<<<<<<< HEAD
   VERSION = '0.0.22'
-=======
-  VERSION = '0.0.18'
->>>>>>> e711d535b61f4597a8f06b313eb61d43a93c3214
 end
--- gems/uc3-dmp-cognito/lib/uc3-dmp-cognito/version.rb
+++ gems/uc3-dmp-cognito/lib/uc3-dmp-cognito/version.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
 module Uc3DmpCognito
-<<<<<<< HEAD
   VERSION = '0.0.14'
-=======
-  VERSION = '0.0.10'
->>>>>>> e711d535b61f4597a8f06b313eb61d43a93c3214
 end
